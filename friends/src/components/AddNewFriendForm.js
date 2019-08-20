db0d94dce78f0a915dd4acc54e5eb36fe8152b73
Red <vhanchar@ucsc.edu>
2019-08-20 14:41:21 -0700

    created initial add new friend form component

---
+++ friends/src/components/AddNewFriendForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddNewFriendForm = () => {
+
+    return (
+        <div className='friend-form'>
+AddNewFriendForm
+        </div>
+    )
+}
+
+export default AddNewFriendForm;
