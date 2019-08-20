9975109ee94f34dbb4b5335bdf0229856d7617f1
Red <vhanchar@ucsc.edu>
2019-08-20 12:50:46 -0700

    created initial loginform component

---
+++ friends/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// components
+import LoginForm from './components/LoginForm';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      app
+      <LoginForm/>
+  
+    </div>
+  );
+}
+
+export default App;
